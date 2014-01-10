177f4df078c39fdab151f798e5e9b0b697a35bcf
paul.lashbrook <lashbrook.paul@gmail.com>
2014-01-10 11:11:21 +0000

    adds first draft of open cart

---
+++ httpdocs/admin/language/english/payment/cardstream.php
@@ -0,0 +1,27 @@
+<?php
+	// Heading
+	$_['heading_title'] = 'Cardstream';
+
+	// Text
+	$_['text_payment']    = 'Payment';
+	$_['text_success']    = 'Success: You have modified Cardstream account details!';
+	$_['text_cardstream'] =
+		'<a onclick="window.open(\'https://www.cardstream.com\');"><img src="view/image/payment/cardstream.png" alt="Cardstream" title="Cardstream" style="border: 1px solid #EEEEEE;" /></a>';
+
+	// Entrys
+	$_['entry_merchantid']     = 'Merchant ID:';
+	$_['entry_merchantsecret'] = 'Merchant Secret:';
+	$_['entry_countrycode']    = 'Country Code';
+	$_['entry_currencycode']   = 'Currency Code';
+
+	$_['entry_order_status'] = 'Order Status:';
+	$_['entry_geo_zone']     = 'Geo Zone:';
+	$_['entry_status']       = 'Status:';
+	$_['entry_sort_order']   = 'Sort Order:';
+
+	// Errors
+	$_['error_permission']     = 'Warning: You do not have permission to modify payment CharityClear!';
+	$_['error_merchantid']     = 'Merchant ID Required!';
+	$_['error_merchantsecret'] = 'Merchant Secret Required!';
+	$_['error_countrycode']    = 'Valid country code required - must be numeric';
+	$_['error_currencycode']   = 'Valid currency code required - must be numeric';
