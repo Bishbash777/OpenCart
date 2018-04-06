60b4472cb79d0d6f12f2b321e5d6df125ebf60e0
jaycemackey <jayce.mackey@gmail.com>
2018-04-06 11:23:05 +0100

    Fixed apostrophe bug
    Fixes bug if a user passes an apostrophe in the address field.

--- httpdocs/catalog/controller/extension/payment/cardstream.php
+++ httpdocs/catalog/controller/extension/payment/cardstream.php
@@ -84,6 +84,8 @@ class ControllerExtensionPaymentCardstream extends Controller
 		foreach ($addressFields as $item) {
 			$bill_addr .= $order[$item] . ($item == 'payment_country' ? "" : ",\n");
 		}
+		
+		$bill_addr = html_entity_decode($bill_addr); 
 
 		$formdata = array(
 			"merchantID"        => $merchant_id,
